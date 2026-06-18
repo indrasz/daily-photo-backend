c89eb31f2838bb4f51180321c078ec0483934ce8
wahyuindra712 <wahyuindra712@gmail.com>
2026-06-18 10:00:19 +0700

    add vercel domain

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ origin: 'http://localhost:3001' });
+  app.enableCors({
+    origin: ['http://localhost:3001', 'https://daily-photo-self.vercel.app'],
+  });
   app.useWebSocketAdapter(new IoAdapter(app));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
